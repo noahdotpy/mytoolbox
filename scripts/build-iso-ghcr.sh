d37cd7d7963e0ddcc4f84b993b90302b0fb9de3f
noahdotpy <noahdotpy@proton.me>
2024-07-03 19:51:36 +1000

    feat(just): change iso build file name

--- scripts/build-iso-ghcr.sh
+++ scripts/build-iso-ghcr.sh
@@ -14,14 +14,25 @@ else
     exit 1
 fi
 
-fedora_version=$(skopeo inspect docker://ghcr.io/noahdotpy/${image_name}:${image_tag} | jq -r '.Labels["org.opencontainers.image.version"]' | awk -F '.' '{print $1}')
+# 40
+fedora_major_version=$(skopeo inspect docker://ghcr.io/noahdotpy/${image_name}:${image_tag} | jq -r '.Labels["org.opencontainers.image.version"]' | awk -F '.' '{print $1}')
+
+# 20240703
+date=$(date +%Y%m%d)
+
+file_name=$(echo "noahdotpy-${image_name}-${image_tag}-${fedora_major_version}-built${date}")
 
 echo "image_name: $image_name"
 echo "image_tag: $image_tag"
 echo "installer_variant: $installer_variant"
-echo "fedora_version: $fedora_version"
+echo "fedora_major_version: $fedora_major_version"
+echo "file_name: $file_name"
+echo "date: $date"
 
-mkdir ./build
+if ! [ -d ./build ]; then
+  mkdir ./build
+fi
+
 sudo podman run --rm --privileged --volume ./build:/build-container-installer/build --security-opt label=disable --pull=newer \
 ghcr.io/jasonn3/build-container-installer:latest \
 ARCH="x86_64" \
@@ -32,7 +43,7 @@ ENROLLMENT_PASSWORD="fedora" \
 IMAGE_NAME="${image_name}" \
 IMAGE_REPO="ghcr.io/noahdotpy" \
 IMAGE_TAG="${image_tag}" \
-ISO_NAME="build/${image_name}-${image_tag}-ghcr.iso" \
+ISO_NAME="build/${file_name}.iso" \
 SECURE_BOOT_KEY_URL='https://github.com/ublue-os/akmods/raw/main/certs/public_key.der' \
 VARIANT="${installer_variant}" \
-VERSION="${fedora_version}"
+VERSION="${fedora_major_version}"
