9a9929843b994bf1ca471bb41fd53493927faca7
noahdotpy <noahdotpy@proton.me>
2024-07-08 23:07:14 +1000

    fix: use correct dir in imagepinned-etcs

--- modules/image-pinned-etcs/image-pinned-etcs.sh
+++ modules/image-pinned-etcs/image-pinned-etcs.sh
@@ -9,7 +9,7 @@ FILES_TO_LINK=($(fd --type file --base-directory $CONFIG_DIRECTORY/image-pinned-
 cp -rf $CONFIG_DIRECTORY/image-pinned-etcs /usr/share/ublue-os/image-pinned-etcs/
 
 for dir in "${DIRS_TO_CREATE[@]}"; do
-	mkdir -p /etc/$dir
+	mkdir -p /usr/etc/$dir
 done
 
 for file in "${FILES_TO_LINK[@]}"; do
