146049e327775844c9d141b08c544e10033734bc
noahdotpy <noahdotpy@proton.me>
2024-07-12 07:45:50 +1000

    fix: use correct branding for fastfetch, use large as default

--- config/image-pinned-etcs/common/profile.d/alias-fastfetch.sh
+++ config/image-pinned-etcs/common/profile.d/alias-fastfetch.sh
@@ -1,2 +1,15 @@
 #!/usr/bin/sh
-alias fastfetch="/usr/bin/fastfetch -c /usr/share/ublue-os/ublue-os.jsonc --logo none"
+IMAGE_NAME=$(cat /usr/share/ublue-os/image-info.json | jq -r '.["image-name"]')
+
+if [[ "$IMAGE_NAME" =~ "bluefin" ]]; then
+	FETCH_LOGO="$(/usr/bin/find /usr/share/ublue-os/bluefin-logos/symbols/* | /usr/bin/shuf -n 1)"
+elif [[ "$IMAGE_NAME" =~ "aurora" ]]; then
+	FETCH_LOGO="/usr/share/ublue-os/fastfetch-logos/aurora-fastfetch-logo"
+else
+	FETCH_LOGO="/usr/share/ublue-os/fastfetch-logos/ublue-fastfetch-logo"
+fi
+
+alias smallfetch="/usr/bin/fastfetch -c /usr/share/ublue-os/fastfetch/configs/small.jsonc --logo ${FETCH_LOGO}"
+alias bigfetch="/usr/bin/fastfetch -c /usr/share/ublue-os/fastfetch/configs/large.jsonc --logo ${FETCH_LOGO}"
+
+alias fastfetch="bigfetch"
