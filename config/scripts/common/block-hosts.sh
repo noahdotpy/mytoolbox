1653b617d6859a1410cd52b0b5ea5a31b209a3c5
noahdotpy <noahdotpy@proton.me>
2024-07-15 19:19:40 +1000

    fix: block-hosts.sh not puts a symlink at /etc/hosts.d/...

--- config/scripts/common/block-hosts.sh
+++ config/scripts/common/block-hosts.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
+FILE_DOWNLOAD="https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts"
 OUTPUT_FILE="/usr/share/bluebuild/image-pinned-etcs/hosts"
+SYMLINK_FILE="/usr/etc/hosts.d/stevenblack-hosts.conf"
 
-wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts -O $OUTPUT_FILE
-echo "Creating symlink at /usr/etc/hosts that points to $OUTPUT_FILE"
-ln -s $OUTPUT_FILE /usr/etc/hosts
+wget $FILE_DOWNLOAD -O $OUTPUT_FILE
+echo "Creating symlink at $SYMLINK_FILE that points to $OUTPUT_FILE"
+ln -s $OUTPUT_FILE $SYMLINK_FILE
