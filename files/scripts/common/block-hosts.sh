c6543862b5fe345e78e5e8f456fc6655ab553e22
noahdotpy <noahdotpy@proton.me>
2024-08-27 13:52:55 +1000

    feat: add /usr/share/hosts functionality to hosts.d service
    moved stevenblack hosts block to /usr/share/hosts

--- files/scripts/common/block-hosts.sh
+++ files/scripts/common/block-hosts.sh
@@ -3,11 +3,7 @@
 set -euo pipefail
 
 FILE_DOWNLOAD="https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts"
-OUTPUT_FILE="/usr/share/bluebuild/image-pinned-etcs/hosts"
-SYMLINK_FILE="/etc/hosts.d/stevenblack-hosts.conf"
+OUTPUT_FILE="/usr/share/hosts/stevenblack-hosts.conf"
 
-mkdir -p $(dirname $SYMLINK_FILE)
-
+mkdir -p $(dirname $OUTPUT_FILE)
 wget $FILE_DOWNLOAD -O $OUTPUT_FILE
-echo "Creating symlink at $SYMLINK_FILE that points to $OUTPUT_FILE"
-ln -s $OUTPUT_FILE $SYMLINK_FILE
