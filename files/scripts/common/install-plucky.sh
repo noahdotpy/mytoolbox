9cb1e17d0c61130c13027a19119f7993a7f152eb
noahdotpy <noahdotpy@proton.me>
2024-10-23 20:21:02 +1000

    feat: install plucky to the image (hopefully fixed now)

---
+++ files/scripts/common/install-plucky.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+/tmp/plucky-install
+
+echo "Creating symlinks to fix pluck - which installs to /opt"
+
+# Create symlink for /opt to /var/opt since it is not created in the image yet
+mkdir -p "/var/opt"
+ln -s "/var/opt" "/opt"
+
+# Create symlinks for pluck directory
+mkdir -p "/usr/lib/opt/pluck"
+ln -s "../../usr/lib/opt/pluck" "/var/opt/pluck"
+echo "Created symlinks to fix /opt/pluck"
