d51253f9f787164157ae625f2fe4f5765c21d616
noahdotpy <noahdotpy@proton.me>
2024-10-29 18:56:40 +1000

    chore: dont do that no more

--- files/scripts/common/block-hosts.sh
+++ files/scripts/common/block-hosts.sh
@@ -5,8 +5,5 @@ set -euo pipefail
 FILE_DOWNLOAD="https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts"
 OUTPUT_FILE="/usr/share/hosts/stevenblack-hosts.conf"
 
-# Unblock
-sed '/roblox.com/d' $OUTPUT_FILE
-
 mkdir -p $(dirname $OUTPUT_FILE)
 wget $FILE_DOWNLOAD -O $OUTPUT_FILE
