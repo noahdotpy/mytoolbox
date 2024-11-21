545c2a9e40073ffcf91f647199b3ed637f692049
noahdotpy <noahdotpy@proton.me>
2024-11-21 16:59:04 +1000

    fix: only sed org.gnome.Ptyxis.desktop to change name to Terminal if file exists

--- files/scripts/common/rename-ptyxis-to-terminal.sh
+++ files/scripts/common/rename-ptyxis-to-terminal.sh
@@ -2,4 +2,10 @@
 
 set -euo pipefail
 
-sed -i 's/Ptyxis/Terminal/g' /usr/share/applications/org.gnome.Ptyxis.desktop
+# Check if the desktop file exists
+if [[ -f /usr/share/applications/org.gnome.Ptyxis.desktop ]]; then
+	sed -i 's/Ptyxis/Terminal/g' /usr/share/applications/org.gnome.Ptyxis.desktop
+else
+	echo "Desktop file does not exist: /usr/share/applications/org.gnome.Ptyxis.desktop"
+	echo "Skipped in-place editing from Ptyxis to Terminal of above desktop file"
+fi
